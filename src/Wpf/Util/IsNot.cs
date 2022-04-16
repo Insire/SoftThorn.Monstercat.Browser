e3d266d8e43067ef5b5c5352d63ce100ab0e4274
Peter Vietense <peter.vietense@hotmail.de>
2022-04-16 18:07:15 +0200

    added fadein animations for images in shell

---
+++ src/Wpf/Util/IsNot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SoftThorn.Monstercat.Browser.Wpf
+{
+    /// <summary>
+    /// negate a boolean value
+    /// </summary>
+    /// <remarks>
+    /// <c>xmlns:mvvm="http://SoftThorn.MvvmScarletToolkit.com/winfx/xaml/shared"</c>
+    /// </remarks>
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public sealed class IsNot : ConverterMarkupExtension<IsNot>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolean)
+            {
+                return !boolean;
+            }
+
+            return false;
+        }
+    }
+}
